f32997326951df840864ceae198826190220af14
HemavathiSundar <148438501+HemavathiSundar@users.noreply.github.com>
2024-04-07 23:33:31 +0530

    Update main.tf

--- AKS/main.tf
+++ AKS/main.tf
@@ -1,47 +1,50 @@
-
-# Generate random resource group name
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "devrelasaservice"
+    storage_account_name = "terraformprod92"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
 
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  name     = random_pet.rg_name.id
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+  }
 }
 
-resource "random_pet" "azurerm_kubernetes_cluster_name" {
-  prefix = "cluster"
+provider "azurerm" {
+  features {}
 }
 
-resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
-  prefix = "dns"
+resource "azurerm_kubernetes_cluster" "k8squickstart" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = "${var.name}-dns01"
+
+  kubernetes_version = var.k8s_version
+
+  
+  network_profile {
+  network_plugin = "azure"
+  network_policy = "azure"
 }
 
-resource "azurerm_kubernetes_cluster" "k8s" {
-  location            = azurerm_resource_group.rg.location
-  name                = random_pet.azurerm_kubernetes_cluster_name.id
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_A2_v2"
+    
+  }
 
   identity {
     type = "SystemAssigned"
   }
 
-  default_node_pool {
-    name       = "agentpool"
-    vm_size    = "Standard_D2s_v3"
-    node_count = var.node_count
-  }
-  linux_profile {
-    admin_username = var.username
-
-    ssh_key {
-      key_data = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
-    }
-  }
-  network_profile {
-    network_plugin    = "kubenet"
-    load_balancer_sku = "standard"
+  tags = {
+    Environment = "Production"
   }
 }
-
